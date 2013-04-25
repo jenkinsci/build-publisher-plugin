16df96830e3c34e41add7d1877303155b30b5013
lvotypko <lvotypko@redhat.com>
2013-04-25 10:34:06 +0200

    Run publish instance in the same JVM.

---
+++ src/test/java/hudson/plugins/build_publisher/JenkinsPublisherRule.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hudson.plugins.build_publisher;
+
+import hudson.model.Hudson;
+import jenkins.model.Jenkins;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.powermock.reflect.Whitebox;
+
+/**
+ *
+ * @author lucinka
+ */
+public class JenkinsPublisherRule extends JenkinsRule{
+    
+    public void before() throws Throwable{
+        super.before();
+        Whitebox.setInternalState(jenkins, "theInstance", null, Jenkins.class);
+    }
+    
+    public void after() {
+        Whitebox.setInternalState(jenkins, "theInstance", jenkins, Jenkins.class);
+        super.after();
+        Whitebox.setInternalState(jenkins, "theInstance", null, Jenkins.class);
+    }
+}
